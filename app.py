534707f48ebd34ea36a70e82dbe0c43a1453dfdb
Nishida Ryosuke <55966582+24daryo@users.noreply.github.com>
2023-09-15 09:31:39 +0900

    push

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+
 app = Flask('hello-world')
+
 @app.route('/')
 def hello():
- return "CloudageSkill Microservices\n"
+  return "ハローワールド\n"
+
 if __name__ == '__main__':
- app.run(host = '0.0.0.0', port = 8080)
+  app.run(host = '0.0.0.0', port = 8080)
